1ffba529f04801f67de88722230e54a016f5da39
Yasin Uslu <nepjua@gmail.com>
2015-02-17 17:00:15 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  name: 'yasinuslu:lodash',
+  version: '3.2.0',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0.3.1');
+
+  api.addFiles('lodash.js');
+  api.addFiles('export.js');
+
+  api.export('lodash');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('yasinuslu:lodash');
+});
